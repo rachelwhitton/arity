12359de93c2dfd7c222cb85f6538c5551b1d3620
Ryan Powszok <rpowszok@vsapartners.com>
2017-11-08 20:25:25 -0600

    Add better defined conditions. Hopefully

--- config/application.php
+++ config/application.php
@@ -37,12 +37,9 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
  * Set up our global environment constant and load its config first
  * Default: production
  */
-if (!defined('WP_ENV')) {
-    define('WP_ENV', env('WP_ENV') ?: 'production');
-}
-if (!defined('IS_LOCAL')) {
-    define('IS_LOCAL', env('IS_LOCAL') ?: false);
-}
+
+defined('WP_ENV') || define('WP_ENV', env('WP_ENV') ?: 'production');
+defined('IS_LOCAL') || define('IS_LOCAL', env('IS_LOCAL') ?: false);
 
 $env_config = __DIR__ . '/environments/' . WP_ENV . '.php';
 if (file_exists($env_config)) {
@@ -67,53 +64,25 @@ define('WP_CONTENT_URL', WP_HOME . CONTENT_DIR);
 /**
  * DB settings
  */
-if (!defined('DB_NAME')) {
-    define('DB_NAME', env('DB_NAME'));
-}
-if (!defined('DB_USER')) {
-    define('DB_USER', env('DB_USER'));
-}
-if (!defined('DB_PASSWORD')) {
-    define('DB_PASSWORD', env('DB_PASSWORD'));
-}
-if (!defined('DB_HOST')) {
-    define('DB_HOST', env('DB_HOST') ?: 'localhost');
-}
-if (!defined('DB_CHARSET')) {
-    define('DB_CHARSET', env('DB_CHARSET') ?: 'utf8mb4');
-}
-if (!defined('DB_COLLATE')) {
-    define('DB_COLLATE', env('DB_COLLATE') ?: 'utf8mb4_unicode_ci');
-}
+defined('DB_NAME') || define('DB_NAME', env('DB_NAME'));
+defined('DB_USER') || define('DB_USER', env('DB_USER'));
+defined('DB_PASSWORD') || define('DB_PASSWORD', env('DB_PASSWORD'));
+defined('DB_HOST') || define('DB_HOST', env('DB_HOST') ?: 'localhost');
+defined('DB_CHARSET') || define('DB_CHARSET', env('DB_CHARSET') ?: 'utf8mb4');
+defined('DB_COLLATE') || define('DB_COLLATE', env('DB_COLLATE') ?: 'utf8mb4_unicode_ci');
 $table_prefix = env('DB_PREFIX') ?: 'wp_';
 
 /**
  * Authentication Unique Keys and Salts
  */
-if (!defined('AUTH_KEY')) {
-    define('AUTH_KEY', env('AUTH_KEY'));
-}
-if (!defined('SECURE_AUTH_KEY')) {
-    define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
-}
-if (!defined('LOGGED_IN_KEY')) {
-    define('LOGGED_IN_KEY', env('LOGGED_IN_KEY'));
-}
-if (!defined('NONCE_KEY')) {
-    define('NONCE_KEY', env('NONCE_KEY'));
-}
-if (!defined('AUTH_SALT')) {
-    define('AUTH_SALT', env('AUTH_SALT'));
-}
-if (!defined('SECURE_AUTH_SALT')) {
-    define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
-}
-if (!defined('LOGGED_IN_SALT')) {
-    define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
-}
-if (!defined('NONCE_SALT')) {
-    define('NONCE_SALT', env('NONCE_SALT'));
-}
+defined('AUTH_KEY') || define('AUTH_KEY', env('AUTH_KEY'));
+defined('SECURE_AUTH_KEY') || define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
+defined('LOGGED_IN_KEY') || define('LOGGED_IN_KEY', env('LOGGED_IN_KEY'));
+defined('NONCE_KEY') || define('NONCE_KEY', env('NONCE_KEY'));
+defined('AUTH_SALT') || define('AUTH_SALT', env('AUTH_SALT'));
+defined('SECURE_AUTH_SALT') || define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
+defined('LOGGED_IN_SALT') || define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
+defined('NONCE_SALT') || define('NONCE_SALT', env('NONCE_SALT'));
 
 /**
  * Validate Authentication Keys and Salts are being used
@@ -144,30 +113,30 @@ if (!defined('WP_DEBUG')) {
     define('WP_DEBUG', $debug);
 }
 
-define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?: false);
-define('WP_DEBUG_DISPLAY', env('WP_DEBUG_DISPLAY') ?: $debug);
-define('SCRIPT_DEBUG', env('SCRIPT_DEBUG') ?: $debug);
-define('SAVEQUERIES', env('SAVEQUERIES') ?: $debug);
+defined('WP_DEBUG_LOG') || define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?: false);
+defined('WP_DEBUG_DISPLAY') || define('WP_DEBUG_DISPLAY', env('WP_DEBUG_DISPLAY') ?: $debug);
+defined('SCRIPT_DEBUG') || define('SCRIPT_DEBUG', env('SCRIPT_DEBUG') ?: $debug);
+defined('SAVEQUERIES') || define('SAVEQUERIES', env('SAVEQUERIES') ?: $debug);
 
 /**
  * Revisions Settings
  */
-define('AUTOSAVE_INTERVAL', env('AUTOSAVE_INTERVAL') ?: 300);
-define('EMPTY_TRASH_DAYS', env('EMPTY_TRASH_DAYS') ?: 14);
-define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?: 5);
+defined('AUTOSAVE_INTERVAL') || define('AUTOSAVE_INTERVAL', env('AUTOSAVE_INTERVAL') ?: 300);
+defined('EMPTY_TRASH_DAYS') || define('EMPTY_TRASH_DAYS', env('EMPTY_TRASH_DAYS') ?: 14);
+defined('WP_POST_REVISIONS') || define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?: 5);
 
 /**
  * Custom Settings
  */
-define('DISALLOW_FILE_MODS', env('DISALLOW_FILE_MODS') ?: false);
-define('DISALLOW_FILE_EDIT', env('DISALLOW_FILE_EDIT') ?: true);
-define('IMAGE_EDIT_OVERWRITE', env('IMAGE_EDIT_OVERWRITE') ?: true);
-define('AUTOMATIC_UPDATER_DISABLED', env('AUTOMATIC_UPDATER_DISABLED') ?: true);
-define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false); // If WP Cron is disabled, it is required to setup a separate cron job
-define('DISABLE_CACHE', env('DISABLE_CACHE') ?: null);
-define('ENABLE_CACHE', env('ENABLE_CACHE') ?: false);
-define('FORCE_SSL_ADMIN', env('FORCE_SSL_ADMIN') ?: false);
-define('DISABLE_ADMIN', env('DISABLE_ADMIN') ?: false);
+defined('DISALLOW_FILE_MODS') || define('DISALLOW_FILE_MODS', env('DISALLOW_FILE_MODS') ?: false);
+defined('DISALLOW_FILE_EDIT') || define('DISALLOW_FILE_EDIT', env('DISALLOW_FILE_EDIT') ?: true);
+defined('IMAGE_EDIT_OVERWRITE') || define('IMAGE_EDIT_OVERWRITE', env('IMAGE_EDIT_OVERWRITE') ?: true);
+defined('AUTOMATIC_UPDATER_DISABLED') || define('AUTOMATIC_UPDATER_DISABLED', env('AUTOMATIC_UPDATER_DISABLED') ?: true);
+defined('DISABLE_WP_CRON') || define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false); // If WP Cron is disabled, it is required to setup a separate cron job
+defined('DISABLE_CACHE') || define('DISABLE_CACHE', env('DISABLE_CACHE') ?: null);
+defined('ENABLE_CACHE') || define('ENABLE_CACHE', env('ENABLE_CACHE') ?: false);
+defined('FORCE_SSL_ADMIN') || define('FORCE_SSL_ADMIN', env('FORCE_SSL_ADMIN') ?: false);
+defined('DISABLE_ADMIN') || define('DISABLE_ADMIN', env('DISABLE_ADMIN') ?: false);
 
 /**
  * Bootstrap WordPress
