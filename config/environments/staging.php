574a8bd932692bd2f7dd6c2a5b6b51f72ad4f747
Ryan Powszok <rpowszok@vsapartners.com>
2017-11-14 19:18:17 -0600

    Turn on wp admin for staging env

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -7,4 +7,3 @@ define('SCRIPT_DEBUG', false);
 
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', env('DISALLOW_FILE_MODS') !== null ? env('DISALLOW_FILE_MODS') : true);
-define('DISABLE_ADMIN', ( env('DISABLE_ADMIN') !== null ? env('DISABLE_ADMIN') : true ));
