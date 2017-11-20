c9159856b1c7531b63786609e046fcd4b327bd7e
Ryan Powszok <rpowszok@vsapartners.com>
2017-11-20 15:29:20 -0600

    Add google search console verification code

--- web/wp-content/themes/arity/resources/templates/layout/head.html.php
+++ web/wp-content/themes/arity/resources/templates/layout/head.html.php
@@ -7,6 +7,7 @@ namespace App\Theme;
   <meta charset="<?= get_bloginfo('charset'); ?>">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="msapplication-config" content="none"/>
+  <meta name="google-site-verification" content="VJohIR_Gmk-5Dd3pGQFA2wkseBfX3jbquvwkI1GsdK4" />
   <meta name="msvalidate.01" content="DB78D25556E2C1E8350F7FF5D7740943" />
 
 <?php do_action('theme/head'); ?>
