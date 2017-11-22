380c9bf7ff5cc54d56c7be4d23701ebea5ff8e4a
Ryan Powszok <rpowszok@vsapartners.com>
2017-11-22 17:24:57 -0600

    Updates to site header. Add mobile toolbar bottom

--- web/wp-content/themes/arity/resources/templates/layout/header.html.php
+++ web/wp-content/themes/arity/resources/templates/layout/header.html.php
@@ -17,9 +17,9 @@ namespace App\Theme;
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
           <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
+          <span class="navbar-toggle-box">
+            <span class="navbar-toggle-inner"></span>
+          </span>
         </button>
         <a href="<?= home_url('/'); ?>" rel="home" title="Arity Homepage" class="navbar-brand">
           <img src="<?= asset_path('img/logo-arity-white.svg'); ?>" alt="" class="navbar-logo">
@@ -27,20 +27,35 @@ namespace App\Theme;
         </a>
       </div>
       <div id="navbar" class="navbar-collapse collapse">
-        <?php
-          if (has_nav_menu('header_primary')) :
-            wp_nav_menu([
-              'menu'            => 'nav_menu',
-              'theme_location'  => 'header_primary',
-              'depth'           => 2,
-              'container'       => false,
-              'menu_class'      => 'nav navbar-nav',
-              'menu_id'         => '',
-              'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
-              // 'walker'          => new NavWalker()
-            ]);
-          endif;
-        ?>
+        <div class="navbar-collapse-wrapper">
+          <div class="navbar-nav-title" aria-hidden="true">
+            <span>Menu</span>
+          </div>
+          <?php
+            if (has_nav_menu('header_primary')) :
+              wp_nav_menu([
+                'menu'            => 'nav_menu',
+                'theme_location'  => 'header_primary',
+                'depth'           => 2,
+                'container'       => false,
+                'menu_class'      => 'nav navbar-nav',
+                'menu_id'         => '',
+                'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
+                // 'walker'          => new NavWalker()
+              ]);
+            endif;
+
+            $menus = get_nav_menu_locations();
+            $nav_items = wp_get_nav_menu_items($menus['header_primary']);
+            $last_nav_item = end($nav_items);
+            $last_nav_item->post_slug = sanitize_title($last_nav_item->title);
+          ?>
+          <div class="navbar-toolbar-bottom" aria-hidden="true">
+            <ul class="nav">
+              <li class="menu-item menu-<?= $last_nav_item->post_slug; ?>"><a href="<?= $last_nav_item->url; ?>"><?= $last_nav_item->title; ?></a></li>
+            </ul>
+          </div>
+        </div><!--/.nav-collapse-wrapper -->
       </div><!--/.nav-collapse -->
     </div>
   </nav>
