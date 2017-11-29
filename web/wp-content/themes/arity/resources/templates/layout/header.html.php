47e4aabb78ce87feaa5827e7a2da2a57c9a0999b
Ryan Powszok <rpowszok@vsapartners.com>
2017-11-29 13:39:35 -0600

    Update site header to new v2 template

--- web/wp-content/themes/arity/resources/templates/layout/header.html.php
+++ web/wp-content/themes/arity/resources/templates/layout/header.html.php
@@ -14,21 +14,21 @@ namespace App\Theme;
 <div class="site-header">
   <nav class="navbar" role="navigation">
     <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="navbar-toggle-box">
-            <span class="navbar-toggle-inner"></span>
-          </span>
-        </button>
-        <a href="<?= home_url('/'); ?>" rel="home" title="Arity Homepage" class="navbar-brand">
-          <img src="<?= asset_path('img/logo-arity-white.svg'); ?>" alt="" class="navbar-logo">
+      <div class="navbar__header">
+        <a role="menuitem" class="navbar__brand" href="<?= home_url('/'); ?>" rel="home" title="Arity Homepage">
+          <img src="<?= asset_path('img/logo-arity-white.svg'); ?>" alt="" class="navbar__logo">
           <span class="sr-only"><?= get_bloginfo('name'); ?></span>
         </a>
+        <button type="button" role="button" class="navbar__toggle collapsed" data-toggle="collapsed" data-target="#navbar" aria-expanded="false" aria-controls="navbar" aria-hidden="true">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="navbar__toggle__box">
+            <span class="navbar__toggle__inner"></span>
+          </span>
+        </button>
       </div>
-      <div id="navbar" class="navbar-collapse collapse">
-        <div class="navbar-collapse-wrapper">
-          <div class="navbar-nav-title" aria-hidden="true">
+      <div id="navbar" class="navbar__collapse collapsed">
+        <div class="navbar__collapse-wrapper">
+          <div class="navbar__nav-title" aria-hidden="true" hidden>
             <span>Menu</span>
           </div>
           <?php
@@ -38,7 +38,7 @@ namespace App\Theme;
                 'theme_location'  => 'header_primary',
                 'depth'           => 2,
                 'container'       => false,
-                'menu_class'      => 'nav navbar-nav',
+                'menu_class'      => 'nav navbar__nav',
                 'menu_id'         => '',
                 'items_wrap'      => '<ul class="%2$s">%3$s</ul>'
               ]);
@@ -49,13 +49,53 @@ namespace App\Theme;
             $last_nav_item = end($nav_items);
             $last_nav_item->post_slug = sanitize_title($last_nav_item->title);
           ?>
-          <div class="navbar-toolbar-bottom" aria-hidden="true">
-            <ul class="nav">
-              <li class="menu-item menu-<?= $last_nav_item->post_slug; ?>"><a href="<?= $last_nav_item->url; ?>"><?= $last_nav_item->title; ?></a></li>
+          <div class="navbar__toolbar-bottom" aria-hidden="true">
+            <ul role="menubar" class="nav">
+              <li class="menu-item menu-<?= $last_nav_item->post_slug; ?>">
+                <a href="<?= $last_nav_item->url; ?>" role="menuitem"><?= $last_nav_item->title; ?></a>
+              </li>
             </ul>
           </div>
-        </div><!--/.nav-collapse-wrapper -->
-      </div><!--/.nav-collapse -->
+        </div><!--/.nav__collapse-wrapper -->
+      </div><!--/.nav__collapse -->
     </div>
   </nav>
+  <div class="dropmenu" data-menu-item="industries" tabindex="-1" aria-hidden="true">
+    <div class="dropmenu__container">
+      <div class="dropmenu__arrow"></div>
+      <div class="dropmenu__wrap">
+        <div class="dropmenu__primary">
+          <a href="#" title="Learn more about Industries Solutions" tabindex="-1">
+            <p>See the ways we can shape transportation, together.</p>
+            <p>
+              <span class="button">
+                <svg class="icon-svg" title="" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use></svg>
+                <span class="button__label">View all industries</span>
+              </span>
+            </p>
+          </a>
+        </div>
+        <div class="dropmenu__secondary">
+          <div class="dropmenu__item">
+            <a href="#" title="Learn more about Shared Mobility" tabindex="-1">
+              <h2>Shared Mobility</h2>
+              <p>Recruit and incentivize top drivers, set smarter pricing strategies and accurately predict losses to maximize efficiency.</p>
+            </a>
+          </div>
+          <div class="dropmenu__item">
+            <a href="#" title="Learn more about Automotive" tabindex="-1">
+              <h2>Automotive</h2>
+              <p>Predict and prevent accidents by leveraging historical data and real-time factors like weather and traffic.</p>
+            </a>
+          </div>
+          <div class="dropmenu__item">
+            <a href="#" title="Learn more about Insurance" tabindex="-1">
+              <h2>Insurance</h2>
+              <p>Identify and retain preferred drivers, anticipate loss and price accurately with the most predictive measure of driving risk ever.</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
