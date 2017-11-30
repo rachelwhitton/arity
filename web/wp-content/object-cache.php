424c75e3b722a46983b6a8c3f5636b878a5eecd1
Ryan Powszok <rpowszok@vsapartners.com>
2017-11-30 17:20:24 -0600

    Updates to include better configs for Redis

--- web/wp-content/object-cache.php
+++ web/wp-content/object-cache.php
@@ -8,5 +8,12 @@ if (file_exists($objectCache = WP_CONTENT_DIR . '/mu-plugins/wp-redis/object-cac
 	if ( ! defined('ENABLE_CACHE') || ENABLE_CACHE == false )
 		return;
 
+    $redis_server = array(
+        'host'     => !empty(REDIS_HOST) ? REDIS_HOST : '127.0.0.1',
+        'port'     => !empty(REDIS_PORT) ? REDIS_PORT : 6379,
+        'auth'     => !empty(REDIS_AUTH) ? REDIS_AUTH : '',
+        'database' => !empty(REDIS_DB) ? REDIS_DB : WP_CACHE_KEY_SALT, // Use a unique value to prevent multiple site conflics
+    );
+
     require_once $objectCache;
 }
