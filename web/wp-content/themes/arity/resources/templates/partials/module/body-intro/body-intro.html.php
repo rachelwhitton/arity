a772969d3a2377d46bffa774aacb3b87e94592b7
Memo Ramirez <mramirez@vsapartners.com>
2017-12-05 14:32:32 -0600

    Fixed eight-column body intro

--- web/wp-content/themes/arity/resources/templates/partials/module/body-intro/body-intro.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/body-intro/body-intro.html.php
@@ -15,13 +15,11 @@ namespace App\Theme;
 <div <?php module_class('body-intro'); ?>>
   <div class="container body-intro__block--bkg">
     <div class="body-intro__row">
-      <div class="body-intro__content">
-        <div class="body-intro__sub-container">
-          <?php if (!empty($data['headline'])) : ?>
-            <<?= $data['h_el']; ?> class="type0 typeBold body-column__headline"><?= $data['headline']; ?></<?= $data['h_el']; ?>>
-          <?php endif; ?>
-          <?= apply_filters('the_content', $data['content']); ?>
-        </div>
+      <div class="body-intro body-eight-column-centered">
+        <?php if (!empty($data['headline'])) : ?>
+          <<?= $data['h_el']; ?> class="type0 typeBold body-column__headline"><?= $data['headline']; ?></<?= $data['h_el']; ?>>
+        <?php endif; ?>
+        <?= apply_filters('the_content', $data['content']); ?>
       </div>
     </div>
   </div>
