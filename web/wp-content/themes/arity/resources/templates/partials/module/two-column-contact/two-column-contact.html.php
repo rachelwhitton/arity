23967cfc524cf56fa397fd83f83c50a4842620ad
Ryan Powszok <rpowszok@vsapartners.com>
2017-12-13 15:26:19 -0600

    Remove contact form on homepage

--- web/wp-content/themes/arity/resources/templates/partials/module/two-column-contact/two-column-contact.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/two-column-contact/two-column-contact.html.php
@@ -13,9 +13,21 @@ namespace App\Theme;
 <div <?php module_class('body-two-column body-two-column--contact'); ?>>
   <div class="row">
     <div class="body-two-column__col left--">
-      <?php component('lite-form', [
-        'headline' => $data['left_column_headline']
-      ]); ?>
+      <?php if( !empty(WP_ENV) && in_array(WP_ENV, array('development','staging'))) : ?>
+        <div class="ar-component lite-form">
+          <?php if (!empty($data['left_column_headline'])) : ?>
+            <div class="row">
+              <div class="col lite-form-inner__col">
+                <h3 class="lite-form__title type0"><?= $data['left_column_headline']; ?></h3>
+              </div>
+            </div>
+          <?php endif; ?>
+        </div>
+      <?php else : ?>
+        <?php component('lite-form', [
+          'headline' => $data['left_column_headline']
+        ]); ?>
+      <?php endif; ?>
     </div>
     <div class="body-two-column__col right--">
       <?php if (!empty($data['right_column_headline'])) : ?>
