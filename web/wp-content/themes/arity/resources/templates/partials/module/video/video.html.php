b6b46e02975dfcbd8e14eed2337d37f9cc4c686c
Ryan Powszok <rpowszok@vsapartners.com>
2017-12-15 13:32:39 -0600

    Merging video module

---
+++ web/wp-content/themes/arity/resources/templates/partials/module/video/video.html.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Theme;
+use Video\Video;
+
+?>
+<?php
+/*
+  Template Name:      Video
+  Template Type:      Module
+  Description:        Video
+  Last Updated:       12/15/2017
+  Since:              1.3.0
+*/
+?>
+<div <?php module_class('video-module'); ?>>
+  <div class="container">
+    <figure class="video-wrapper">
+      <?php // the_video($data['url']); ?>
+    </figure>
+  </div>
+</div>
