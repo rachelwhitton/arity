e0900c39c3bec8efe0fd3076481b9518fd82ed89
Memo Ramirez <mramirez@vsapartners.com>
2017-12-15 22:59:17 -0600

    Added invalid styles to email module

--- web/wp-content/themes/arity/resources/templates/partials/module/email-form/email-form.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/email-form/email-form.html.php
@@ -20,14 +20,12 @@ namespace App\Theme;
     <div class="email-only-container">
       <form action="<?= $data['form_url']; ?>" method="POST">
 
-        <div class="form-group">
+        <div class="form-group form-group--required">
           <?php if (!empty($data['headline'])) : ?>
-            <label class="form-group-email-only-label" for="email"><?= $data['headline']; ?></label>
+            <label class="form-group-label" for="email"><?= $data['headline']; ?></label>
           <?php endif; ?>
-          <div class="email-only-input">
-            <input type="email" class="form-control" name="email" id="email" placeholder="Enter your email address">
-            <div class="form-control-feedback" data-error="invalid">Please enter a valid email</div>
-          </div>
+          <input type="email" class="form-control" name="email" id="email" placeholder="Enter your email address">
+          <div class="form-control-feedback" data-error="invalid">Please enter a valid email</div>
         </div>
         <div class="email-only-button-group">
           <?php if ($data['bkg_color'] == 'blue') : ?>
