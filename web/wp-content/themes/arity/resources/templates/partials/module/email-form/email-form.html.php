f61132b334911fb5ac888d98dc2e9e7a7565526b
Ryan Powszok <rpowszok@vsapartners.com>
2018-01-05 12:47:14 -0600

    More updates to email form

--- web/wp-content/themes/arity/resources/templates/partials/module/email-form/email-form.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/email-form/email-form.html.php
@@ -24,6 +24,13 @@ namespace App\Theme;
 
     <div class="email-form__form">
       <form action="<?= $data['form_url']; ?>" method="POST">
+        <?php if(!empty($data['is_salesforce'])) : ?>
+          <input type=hidden name="oid" value="<?= $data['form_oid']; ?>">
+          <input type=hidden name="retURL" value="<?= $data['form_return_url']; ?>">
+          <input type="hidden" name="lead_source" id="input_lead_source" value="Conference / Trade Show">
+          <input type="hidden" name="company" id="input_company" value="CES Astronaut">
+        <?php endif; ?>
+
         <div class="form-group form-group--required">
           <label class="form-group-label" for="input_first_name">First name</label>
           <input type="text" class="form-control" name="first_name" id="input_first_name" autocapitalize="words" placeholder="" required>
@@ -45,12 +52,13 @@ namespace App\Theme;
           <div class="form-control-feedback" data-error="invalid">Please enter a valid email</div>
         </div>
 
-        <input type="hidden" name="company" id="input_company" value="CES Astronaut">
-        <input type="hidden" name="lead_source" id="input_lead_source" value="Conference / Trade Show">
-
         <div class="btn-group">
           <button type="submit" class="button button--primary white-blue-button--">Submit</button>
         </div>
+
+        <?php if(!empty($data['use_captcha'])) : ?>
+          <div class="g-recaptcha" data-size="invisible" data-badge="inline"></div>
+        <?php endif; ?>
       </form>
     </div>
   </div>
