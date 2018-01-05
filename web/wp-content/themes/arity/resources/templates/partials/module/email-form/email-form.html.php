18927b9578d47dd88f0fab3b3819b3d9db68c5d1
Ryan Powszok <rpowszok@vsapartners.com>
2018-01-05 12:57:52 -0600

    Email form updates

--- web/wp-content/themes/arity/resources/templates/partials/module/email-form/email-form.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/email-form/email-form.html.php
@@ -31,21 +31,21 @@ namespace App\Theme;
           <input type="hidden" name="company" id="input_company" value="CES Astronaut">
         <?php endif; ?>
 
-        <div class="form-group form-group--required">
+        <div class="form-group form-group-1 form-group--required">
           <label class="form-group-label" for="input_first_name">First name</label>
-          <input type="text" class="form-control" name="first_name" id="input_first_name" autocapitalize="words" placeholder="" required>
+          <input type="text" class="form-control" name="first_name" id="input_first_name" placeholder="" required>
           <div class="form-control-feedback" data-error="required">Please enter first name</div>
           <div class="form-control-feedback" data-error="invalid">Please enter a valid first name</div>
         </div>
 
-        <div class="form-group form-group--required">
+        <div class="form-group form-group-2 form-group--required">
           <label class="form-group-label" for="input_last_name">Last name</label>
-          <input type="text" class="form-control" name="last_name" id="input_last_name" autocapitalize="words" placeholder="" required>
+          <input type="text" class="form-control" name="last_name" id="input_last_name" placeholder="" required>
           <div class="form-control-feedback" data-error="required">Please enter last name</div>
           <div class="form-control-feedback" data-error="invalid">Please enter a valid last name</div>
         </div>
 
-        <div class="form-group form-group--required">
+        <div class="form-group form-group-3 form-group--required">
           <label class="form-group-label" for="input_email">Email</label>
           <input type="email" class="form-control" name="email" id="input_email" placeholder="" required>
           <div class="form-control-feedback" data-error="required">Please enter email</div>
