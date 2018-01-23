fca4d28f7ae5560ece9b2ac14f73db189991d133
Jan Kordylewski <jkordylewski@vsapartners.com>
2018-01-23 16:04:40 -0600

    blog promo layout fixes

--- web/wp-content/themes/arity/resources/templates/partials/module/blog-promo/blog-promo.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/blog-promo/blog-promo.html.php
@@ -7,16 +7,38 @@ namespace App\Theme;
 <div class="ar-module blog-promo">
   <div class="row">
       <?php foreach ($data['posts'] as $item) : ?>
-        <div class="blog-promo__col">
+        <!-- <div class="blog-promo__col">
           <?php echo get_the_post_thumbnail($item->ID); ?>
           <div class="blog-promo__content">
             <h4><?php echo $item->primary_term; ?></h4>
             <h2><?php echo $item->post_title; ?></h2>
             <?php echo $item->post_excerpt;?>
-            <?php the_date('F Y', '<div class="date">', '</div>'); ?>
+            <?php echo get_the_date('F Y'); ?>
             <div class="">5 min read</div>
           </div>
-    		</div>
+    		</div> -->
+        <div class="blog-card__col col-4">
+          <div class="blog-card__image">
+            <?php if (get_the_post_thumbnail($item->ID )) : ?>
+                <?php echo get_the_post_thumbnail($item->ID, 'post-thumbnail' ); ?>
+            <?php else : ?>
+              <div class="blog-card__filler-img"></div>
+            <?php endif ?>
+          </div>
+          <div class="blog-card__inner">
+            <div class="blog-card__cat">
+              <a href="/insights/category/<?php echo strtolower($item->primary_term) ?>"><?php echo $item->primary_term ?></a>
+            </div>
+            <?php the_title( '<h1 class="blog-card__title">', '</h1>' ); ?>
+            <div class="blog-card__excerpt">
+             <?php the_excerpt(); ?>
+            </div>
+            <div class="blog-card__date">
+              <?php echo get_the_date('F Y'); ?>
+            </div>
+            <div class="">5 min read</div>
+          </div>
+        </div>
       <?php endforeach; ?>
   </div>
 </div>
