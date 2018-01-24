2c83c5248f3cef1243a1443e0b49faef943f3854
Ryan Powszok <rpowszok@vsapartners.com>
2018-01-24 13:02:58 -0600

    Careers Feed updates

--- web/wp-content/themes/arity/resources/templates/partials/module/career-list/career-list.data.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/career-list/career-list.data.php
@@ -1,7 +1,37 @@
 <?php
+namespace App\Theme;
 
 if(empty($data['h_el'])) {
   $data['h_el'] = 'h2';
+}
+
+$data['feed'] = false;
+
+$feed = get_rss('https://jobsearch.allstate.com/Rss/All/Search/jobtitle/arity/');
+if(!empty($feed['channel']['item'])) {
+  $data['feed'] = $feed['channel']['item'];
+
+  foreach($data['feed'] as $i=>$item) {
+
+    // Remove Arity prefix
+    $item['title'] = preg_replace('/Arity-/', '', $item['title']);
+
+    // Convert dashes to spaces
+    $item['title'] = preg_replace('/[\s-]+/', ' ', $item['title']);
+
+    // Set Object Value
+    $data['feed'][$i]['title'] = $item['title'];
+  }
+
+  // Sort by alphabetical
+  $data['feed'] = usort($data['feed'], function($a, $b) {
+    if ($a['title'] == $b['title']) {
+        return 0;
+    }
+    return ($a['title'] < $b['title']) ? -1 : 1;
+  });
+
+  $data['feedCount'] = count($data['feed']);
 }
 
 return $data;
