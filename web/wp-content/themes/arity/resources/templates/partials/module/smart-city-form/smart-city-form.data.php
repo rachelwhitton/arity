07e9a6068da188813dbdba5dacc2fe40502ea0bf
Jan Kordylewski <jkordylewski@vsapartners.com>
2018-02-05 13:53:55 -0600

    moving new form to hotfix again

---
+++ web/wp-content/themes/arity/resources/templates/partials/module/smart-city-form/smart-city-form.data.php
@@ -0,0 +1,30 @@
+<?php
+
+// Production
+if(empty($data['form_url'])) {
+  $data['is_salesforce'] = true;
+  $data['form_url'] = 'https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8';
+  $data['form_oid'] = '00Df4000001TyK5';
+
+  // Set return url
+  global $wp;
+  $data['form_return_url'] = home_url( $wp->request );
+  $data['form_return_url'] = trailingslashit($data['form_return_url']) . '#thank-you';
+
+  // Testing
+  if(!empty(WP_ENV) && WP_ENV != 'production') {
+    $data['form_url'] = 'https://test.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8';
+    $data['form_oid'] = '00DW0000008xyd5';
+  }
+}
+
+$data['use_captcha'] = false;
+
+// Classes
+$data['classes'][] = 'email-form';
+
+if(!empty($data['bkg_color'])) {
+  $data['classes'][] = 'email-form--bkg-dark';
+}
+
+return $data;
