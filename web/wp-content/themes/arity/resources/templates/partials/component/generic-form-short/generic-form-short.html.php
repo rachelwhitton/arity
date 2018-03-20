971e47d6873e4f730e2769f1667415f941b2a7b8
Jan Kordylewski <jkordylewski@vsapartners.com>
2018-03-20 12:26:46 -0500

    latest udpates and fixes

--- web/wp-content/themes/arity/resources/templates/partials/component/generic-form-short/generic-form-short.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/component/generic-form-short/generic-form-short.html.php
@@ -3,30 +3,35 @@ namespace App\Theme;
 ?>
 <?php
 /*
-  Template Name:      Smart City Form
+  Template Name:      Generic Form Short
   Template Type:      Module
-  Description:        smart-city form with option to choose two colors.
+  Description:        short generic form.
   Last Updated:       02/02/2018
   Since:              1.5.0
 */
+
+  $form_oid = '00Dq0000000CQhG';
+  if(!empty(WP_ENV) && WP_ENV != 'production') {
+    // Testing
+    $form_oid = '00Dq0000000CQhG';
+  }
+
+
 ?>
 
 <div class="email-form__form">
   <form action="<?= $data['form_url']; ?>" method="POST">
     <?php if(!empty($data['is_salesforce'])) : ?>
-      <input type="hidden" name="oid" value="<?= $data['form_oid']; ?>">
+      <input type="hidden" name="oid" value="<?= $form_oid; ?>">
       <input type="hidden" name="retURL" value="<?= $data['form_return_url']; ?>">
       <input type="hidden" name="lead_source" value="Arity.com" id="lead_source" />
+
+      <select style="visibility:hidden" id="lead_source" name="lead_source">
+        <option selected value="Conference / Trade Show"/>
+      </select><br>
     <?php endif; ?>
 
-    <div class="form-group form-group-1 form-group--required">
-      <label class="form-group-label" for="input_first_name">First name</label>
-      <input type="text" class="form-control" name="first_name" id="input_first_name" placeholder="" required>
-      <div class="form-control-feedback" data-error="required">Please enter first name</div>
-      <div class="form-control-feedback" data-error="invalid">Please enter a valid name</div>
-    </div>
-
-    <div class="form-group form-group-2 form-group--required">
+    <div class="form-group form-group-3 form-group--required">
       <label class="form-group-label" for="input_last_name">Last name</label>
       <input type="text" class="form-control" name="last_name" id="input_last_name" placeholder="" required>
       <div class="form-control-feedback" data-error="required">Please enter last name</div>
@@ -46,18 +51,6 @@ namespace App\Theme;
       <div class="form-control-feedback" data-error="required">Please enter your company</div>
       <div class="form-control-feedback" data-error="invalid">Please enter a valid company</div>
     </div>
-    <?php
-      $interested_id = '00Nf4000009v5NK';
-      if(!empty(WP_ENV) && WP_ENV != 'production') {
-        // Testing
-        $interested_id = '00Nf4000009v5NK';
-      }
-    ?>
-
-    <select style="visibility:hidden" type="hidden" id="<?=$interested_id; ?>" multiple="multiple" name="<?=$interested_id; ?>" title="Interested In">
-      <option selected value="Government"/>
-      <option selected value="Smart Cities"/>
-    </select>
 
     <div class="btn-group">
       <button type="submit" class="button button--primary white-blue-button--">Submit</button>
--- web/wp-content/themes/arity/resources/templates/partials/module/generic-form-container/generic-form-container.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/generic-form-container/generic-form-container.html.php
@@ -1,6 +1,5 @@
 <?php
 namespace App\Theme;
-var_dump($data);
 ?>
 <?php
 /*
