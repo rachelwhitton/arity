fc19423b480a9ab56c1ce7f0516b7df717c16555
Jan Kordylewski <jkordylewski@vsapartners.com>
2018-04-17 11:12:04 -0500

    archiving lead gen module

---
+++ web/wp-content/themes/arity/resources/templates/partials/component/lead-gen-form/lead-gen-form.html.php
@@ -0,0 +1,139 @@
+<?php
+namespace App\Theme;
+?>
+<?php
+/*
+Template Name:      LeadGen Form
+Template Type:      Module
+Description:        Page block with long form - Lead Gen
+Last Updated:       04/18/2018
+Since:              1.0.0
+*/
+
+  $form_oid = '00Df4000001TyK5';
+  if(!empty(WP_ENV) && WP_ENV != 'production') {
+    // Testing
+    $form_oid = '00Dq0000000CQhG';
+  }
+
+
+?>
+
+<div class="generic-form__wrap generic-form__form">
+  <form id="app__form__form" name="app__form__form" class="leadgen-form-inner" action="<?= $data['form_url']; ?>" method="POST">
+    <input type=hidden name="oid" value="<?= $data['form_oid']; ?>">
+    <input id="recordType" name="recordType" type="hidden" value="01216000000WeRH">
+    <input name="lead_source" type="hidden" value="Web- All Other">
+    <input name="retURL" type="hidden" value="http://test12345.force.com/aritylead/ArityWebLeadThankyou?Sender">
+    <input name="debug" type="hidden" value="0">
+
+    <script>
+        var onRecaptchaSuccess = function(token) {
+          formValidator.onSuccess(token);
+        };
+    </script>
+
+    <div class="leadgen-form-inner__row">
+      <div class="col leadgen-form-inner__col two-col--">
+        <label for="first_name">First Name
+          <span class="required">*</span>&nbsp;&nbsp;
+          <span class="required" id="first_name_required" style="display:none;color:#FA364A;float:right; font-size:14px;">Please enter first name</span>
+          <span class="required" id="first_name_valid" style="display:none;color:#FA364A;float:right;font-size:14px">Please enter a valid first name</span>
+        </label>
+        <input id="first_name" maxlength="40" name="first_name" onkeyup="formValidator.onChange();" size="20" type="text">
+      </div>
+      <div class="col leadgen-form-inner__col two-col--">
+        <label for="last_name">Last Name
+          <span class="required">*</span>&nbsp;&nbsp;
+          <span class="required" id="last_name_required" style="display:none;color:#FA364A;float:right; font-size:14px;">Please enter last name</span>
+          <span class="required" id="last_name_valid" style="display:none;color:#FA364A;float:right;font-size:14px">Please enter a valid last name</span>
+        </label>
+        <input id="last_name" maxlength="80" name="last_name" onkeyup="formValidator.onChange();" size="20" type="text"><br>
+      </div>
+    </div>
+    <div class="leadgen-form-inner__row">
+      <div class="col leadgen-form-inner__col">
+        <label for="company">Company</label>
+        <input id="company" maxlength="40" name="company" size="20" type="text">
+      </div>
+    </div>
+
+    <div class="leadgen-form-inner__row">
+      <div class="col leadgen-form-inner__col">
+        <label for="email">Email<span class="required">*</span>&nbsp;&nbsp;<span class="required" id="email_required" style="display:none;color:#FA364A;float:right; font-size:14px;">Please enter email</span><span class="required" id="email_valid" style="display:none;color:#FA364A;float:right;font-size:14px">Please enter a valid email</span></label>
+        <input id="email" maxlength="80" name="email" onkeyup="formValidator.applyRules();formValidator.onChange();" size="20" type="text"><br>
+      </div>
+    </div>
+    <div class="leadgen-form-inner__row">
+      <div class="col leadgen-form-inner__col">
+        <label for="phone">Phone&nbsp;&nbsp;<span class="required" id="phone_required" style="display:none;color:#FA364A;float:right; font-size:14px;">Phone is required when preferred method is set to phone</span><span class="required" id="phone_valid" style="display:none;color:#FA364A;float:right;font-size:14px">Please enter a valid phone</span></label>
+        <input id="phone" maxlength="40" name="phone" onkeyup="formValidator.applyRules();formValidator.onChange();" size="20" type="text"><br>
+      </div>
+    </div>
+
+    <div class="leadgen-form-inner__row">
+      <div class="col leadgen-form-inner__check-col">
+        <label>Industry</label>
+      </div>
+    </div>
+    <div class="leadgen-form-inner__row">
+      <div class="col leadgen-form-inner__check-col">
+        <span class="checkbox-li"><input id="cb1" name="00N1600000F4WrJ" type="checkbox" value="Insurance"><label class="checkradio" for="cb1"><span></span> Insurance</label>
+        </span>
+      </div>
+      <div class="col leadgen-form-inner__check-col">
+        <span class="checkbox-li"><input id="cb2" name="00N1600000F4WrJ" type="checkbox" value="OEM"><label class="checkradio" for="cb2"><span></span> OEM</label>
+        </span>
+      </div>
+      <div class="col leadgen-form-inner__check-col">
+        <span class="checkbox-li"><input id="cb3" name="00N1600000F4WrJ" type="checkbox" value="Shared Mobility"><label class="checkradio" for="cb3"><span></span> Shared Mobility</label>
+        </span>
+      </div>
+      <div class="col leadgen-form-inner__check-col">
+        <span class="checkbox-li"><input id="cb4" name="00N1600000F4WrJ" type="checkbox" value="Gig Economy"><label class="checkradio" for="cb4"><span></span> Gig Economy</label>
+        </span>
+      </div>
+      <div class="col leadgen-form-inner__check-col">
+        <span class="checkbox-li"><input id="cb5" name="00N1600000F4WrJ" type="checkbox" value="Auto Aftermarket"><label class="checkradio" for="cb5"><span></span> Auto Aftermarket</label>
+        </span>
+      </div>
+      <div class="col leadgen-form-inner__check-col">
+        <span class="checkbox-li"><input id="cb6" name="00N1600000F4WrJ" type="checkbox" value="Government"><label class="checkradio" for="cb6"><span></span> Government</label>
+        </span>
+      </div>
+      <div class="col leadgen-form-inner__check-col">
+        <span class="checkbox-li"><input id="cb7" name="00N1600000F4WrJ" type="checkbox" value="Other"><label class="checkradio" for="cb7"><span></span> Other</label>
+        </span>
+      </div>
+    </div>
+
+    <div class="leadgen-form-inner__row">
+      <div class="col leadgen-form-inner__check-col">
+        <label>Preferred Method of Contact</label>
+      </div>
+    </div>
+
+    <div class="leadgen-form-inner__row">
+
+      <div class="col leadgen-form-inner__check-col">
+        <span class="radio-li"><input checked="checked" id="r1" name="00N1600000F4WrE" onchange="formValidator.onChange();" type="radio" value="Email"><label class="checkradio" for="r1"><span></span> Email</label>&nbsp;&nbsp;</span>
+      </div>
+      <div class="col leadgen-form-inner__check-col">
+        <span class="radio-li"><input id="r2" name="00N1600000F4WrE" onchange="formValidator.onChange();" type="radio" value="Phone"><label class="checkradio" for="r2"><span></span> Phone</label>&nbsp;&nbsp;</span>
+      </div>
+      <div class="col leadgen-form-inner__check-col">
+        <span class="radio-li"><input id="r3" name="00N1600000F4WrE" onchange="formValidator.onChange();" type="radio" value="No Preference"><label class="checkradio" for="r3"><span></span> No Preference</label>
+        </span>
+      </div>
+    </div>
+
+    <div class="leadgen-form-inner__row">
+      <div class="col leadgen-form-inner__check-col">
+        <button id='submit_btn' class="submit-btn">Submit</button>
+        <br /><br />
+        <div id='recaptcha' class="g-recaptcha" data-sitekey="6LemsC8UAAAAABhmM91zrsD0Paw7Uxa2MFChpoKY" data-callback="onRecaptchaSuccess" data-size="invisible" data-badge="inline"></div>
+      </div>
+    </div>
+
+  </form>
+</div>
