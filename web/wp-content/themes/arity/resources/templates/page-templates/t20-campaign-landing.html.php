8e2191534afd7b8db42a77cfec9841c240afb757
Jan Kordylewski <jkordylewski@vsapartners.com>
2018-04-19 15:35:35 -0500

    adding new t20 template

---
+++ web/wp-content/themes/arity/resources/templates/page-templates/t20-campaign-landing.html.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Theme;
+
+?>
+<?php
+/*
+  Template Name:      T20 Campaign Landing Template
+  Template Type:      Page Template
+  Description:
+  Last Updated:       04/20/2018
+  Since:              1.8.1
+*/
+?>
+
+<?php get_header() ?>
+
+<?php do_action('theme/before_content') ?>
+
+<div id="main" class="site-content">
+  <?php the_acf_content(); ?>
+</div>
+
+<?php do_action('theme/after_content') ?>
+
+<?php get_footer() ?>
