6646cacc21f81f1741cc7bffda52a49b8385af3d
Jan Kordylewski <jkordylewski@vsapartners.com>
2018-04-23 13:05:51 -0500

    updates to content image block - simple rename to "Block: 2 Column" added guidance recs and ability for adding a simple headline

--- web/wp-content/themes/arity/resources/templates/partials/module/content-image-block/content-image-block.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/content-image-block/content-image-block.html.php
@@ -15,6 +15,33 @@ namespace App\Theme;
 
 <div <?php module_class($data['classes']); ?>>
   <div class="container">
+    <?php if (!empty($data['module-headline'])) : ?>
+      <div class="container">
+        <div class="row">
+          <div class="">
+            <?php if(!empty($data['eyebrow'])) : ?>
+              <?php element('eyebrow', array(
+                'classes' => 'eyebrow',
+                'label' => $data['eyebrow']
+              )); ?>
+            <?php endif; ?>
+
+            <?php if (!empty($data['module-headline'])) : ?>
+              <?php element('headline', array(
+              'classes' => 'block-highlights__title',
+              'headline' => $data['module-headline']
+            )); ?>
+            <?php endif; ?>
+
+            <?php if(!empty($data['subhead'])) : ?>
+              <div class="block-highlights__subhead">
+                <?= $data['subhead']; ?>
+              </div>
+            <?php endif; ?>
+          </div>
+        </div>
+      </div>
+    <?php endif; ?>
     <div class="row">
       <?php if (!empty($data['image_id'])) : ?>
         <div class="content-image-block__col wide-- content-image-block__img-box">
