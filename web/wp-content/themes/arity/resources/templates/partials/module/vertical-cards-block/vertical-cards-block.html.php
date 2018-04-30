9dc712c589826f980dc491078051dd4a9e550712
Jan Kordylewski <jkordylewski@vsapartners.com>
2018-04-30 15:36:16 -0500

    card block updates layout fixes

--- web/wp-content/themes/arity/resources/templates/partials/module/vertical-cards-block/vertical-cards-block.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/vertical-cards-block/vertical-cards-block.html.php
@@ -14,14 +14,16 @@ namespace App\Theme;
   <div class="vertical-cards-block__row">
     <div class="vertical-cards-block__content">
       <?php if (!empty($data['headline'])) : ?>
+      <div class="container">
         <div class="row">
           <div class="vertical-cards__title">
             <<?= $data['h_el']; ?> class="type3"><?= $data['headline']; ?></<?= $data['h_el']; ?>>
           </div>
         </div>
+      </div>
       <?php endif; ?>
       <div class="container">
-        <div class="row body-eight-column-centered">
+        <div class="row body-inset-ten-col__col">
           <?php foreach ($data['cards'] as $card) : ?>
             <div class="vertical-cards__col">
               <?php the_partial($card); ?>
