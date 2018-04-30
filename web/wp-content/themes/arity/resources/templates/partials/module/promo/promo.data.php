c6f0b4986bd9a9e5d5cda91ccd0b47be0a4079a7
Jan Kordylewski <jkordylewski@vsapartners.com>
2018-04-30 11:00:17 -0500

    fix to how data gets added for promo module

--- web/wp-content/themes/arity/resources/templates/partials/module/promo/promo.data.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/promo/promo.data.php
@@ -5,10 +5,18 @@ $data['classes'][] = 'promo';
 if(!empty($data['padding'])){
   $data['classes'][] = 'promo--padding';
   if(!empty($data['padding'][0])){
-    $data['classes'][] = 'promo--padding-top';
+    if($data['padding'][0]=='top'){
+      $data['classes'][] = 'promo--padding-top';
+    }else{
+      $data['classes'][] = 'promo--padding-bottom';
+    }
   }
   if(!empty($data['padding'][1])){
-    $data['classes'][] = 'promo--padding-bottom';
+    if($data['padding'][1]=='top'){
+      $data['classes'][] = 'promo--padding-top';
+    }else{
+      $data['classes'][] = 'promo--padding-bottom';
+    }
   }
 }
 
