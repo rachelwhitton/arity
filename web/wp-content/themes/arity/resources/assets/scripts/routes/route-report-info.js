9f8dc651b850306d9ba812c92aa4e58f165f0459
Jan Kordylewski <jkordylewski@vsapartners.com>
2018-05-08 12:19:21 -0500

    fix for broken route report nav

--- web/wp-content/themes/arity/resources/assets/scripts/routes/route-report-info.js
+++ web/wp-content/themes/arity/resources/assets/scripts/routes/route-report-info.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 export default {
   init() {
     var $siteContent = $('.site-content');
-    $('.body-intro', $siteContent).first().attr('id', 'features');
+    $('.icon-two-column', $siteContent).first().attr('id', 'features');
     $('.accordion-wrapper', $siteContent).first().attr('id', 'faq');
     $('.action-bar-one-col-cta', $siteContent).first().attr('id', 'support');
   },
