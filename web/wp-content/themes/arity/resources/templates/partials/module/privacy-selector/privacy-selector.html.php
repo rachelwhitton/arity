fb6d60eeceba16eaf3120f6e8484d61311d6e62f
Jan Kordylewski <jkordylewski@vsapartners.com>
2018-06-01 15:14:18 -0500

    cleanup on aisle 4

--- web/wp-content/themes/arity/resources/templates/partials/module/privacy-selector/privacy-selector.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/privacy-selector/privacy-selector.html.php
@@ -15,16 +15,12 @@ namespace App\Theme;
 <div class="ar-module privacy-selector">
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-lg-8 col-lg-offset-4">
-        <div class="row">
-          <div class="col-xs-6 col-lg-4">
-            <form class="">
-              <div class="form-group form-group--required">
-              <?= $data['menu_list']; ?>
-              </div>
-            </form>
+      <div class="privacy-selector__col">
+        <form class="">
+          <div class="form-group">
+          <?= $data['menu_list']; ?>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   </div>
