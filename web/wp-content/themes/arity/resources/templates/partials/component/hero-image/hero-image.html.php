96aa54abd7416f3cd05f3870f80b416f59647a49
Usman Khawaja <ukhawaja@vsapartners.com>
2018-08-29 16:28:18 -0400

    Updated hero-b to have consistent dot-dash overlay

--- web/wp-content/themes/arity/resources/templates/partials/component/hero-image/hero-image.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/component/hero-image/hero-image.html.php
@@ -13,5 +13,29 @@ namespace App\Theme;
 <div <?php component_class('hero-image'); ?><?php if (!empty($data['background-image'])) : ?> style="background-image: url(<?= $data['background-image']; ?>);"<?php endif; ?>></div>
 
 <?php if ($data['overlay']) : ?>
-  <div class="hero-image__overlay"><div class="overlay-inner"></div></div>
+  <!-- div class="hero-image__overlay"><div class="overlay-inner"></div></div -->
+<div class="hero-image__overlay hero-elaborated dots-animate-modified">
+    <ul id="loader">
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    </ul>
+</div>
 <?php endif; ?>
