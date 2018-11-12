83155e9693c4b52887377cb2d7e0b8337c3b83cf
Usman Khawaja <usman.khawaja@ibm.com>
2018-11-12 14:18:53 -0500

    Updated smart-cities-usdot

--- web/wp-content/themes/arity/resources/templates/page-templates/t13-smart-cities-usdot.html.php
+++ web/wp-content/themes/arity/resources/templates/page-templates/t13-smart-cities-usdot.html.php
@@ -11,18 +11,17 @@ namespace App\Theme;
   Since:              1.0.0
 */
 
-echo 'hello';
 //https://www.arity.com//wp-content/uploads/2018/10/image2_visualization_10242018/index.html?rand=1542040997
         // create curl resource 
         $ch = curl_init(); 
-        curl_setopt($ch, CURLOPT_URL, "https://dev.arity/wp-content/uploads/2018/11/smart_cities_prototype/app/index.html#/screens"); 
+        curl_setopt($ch, CURLOPT_URL, "https://arity.com/wp-content/uploads/2018/11/smart_cities_prototype/app/index.html"); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
         curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"); // set browser/user agent 
         echo $output = curl_exec($ch); 
-        
+        /*
         if(curl_exec($ch) === false)
           {
               echo 'Curl error: ' . curl_error($ch);
@@ -31,5 +30,5 @@ echo 'hello';
           {
               echo 'Operation completed without any errors';
           }
-
+*/
         curl_close($ch);
