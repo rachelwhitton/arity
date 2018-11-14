885b3f51f16088073cb3890fd542d6fc7669adfd
Usman Khawaja <usman.khawaja@ibm.com>
2018-11-14 16:11:26 -0500

    block 2 col form

--- web/wp-content/themes/arity/resources/templates/partials/module/content-image-block/content-image-block.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/content-image-block/content-image-block.html.php
@@ -10,7 +10,7 @@ namespace App\Theme;
   Last Updated:       08/01/2017
   Since:              1.0.0
 */
-//echo '<pre>'; print_r($data); echo '</pre>';
+//echo '<pre>'; print_r($data); echo '</pre>'; 
 
 $class = 'content-image-block';
 if($data['content-chooser']=='layout__form'){
