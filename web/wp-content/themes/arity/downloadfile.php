89c836de356090e3b0e44177ce3cf8a7070285c7
Usman Khawaja <ukhawaja@vsapartners.com>
2018-11-30 14:52:25 -0500

    Code test for private json

--- web/wp-content/themes/arity/downloadfile.php
+++ web/wp-content/themes/arity/downloadfile.php
@@ -1,6 +1,6 @@
 <?php
 if ((isset($_ENV['PANTHEON_ENVIRONMENT']) && $_ENV['PANTHEON_ENVIRONMENT'] == 'live') ||(isset($_ENV['PANTHEON_ENVIRONMENT']) && $_ENV['PANTHEON_ENVIRONMENT'] == 'dev')) {
-    $json_text = file_get_contents('sites/default/files/private/dev.json');
+    $json_text = file_get_contents('wp-content/uploads/private/dev.json');
     $stripe_data = json_decode($json_text, TRUE);
     echo '<pre>';print_r($stripe_data);echo '</pre>';  
   }
