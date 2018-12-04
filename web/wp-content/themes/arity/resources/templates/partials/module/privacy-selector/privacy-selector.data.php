7162cb6011c4c94b0d89bc862c39420d421454b9
Usman Khawaja <ukhawaja@vsapartners.com>
2018-12-04 12:25:31 -0500

    Privacy Langusge Selector

--- web/wp-content/themes/arity/resources/templates/partials/module/privacy-selector/privacy-selector.data.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/privacy-selector/privacy-selector.data.php
@@ -8,7 +8,7 @@ if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name 
     $menu_items = wp_get_nav_menu_items($menu->term_id);
 
     $data['menu_list'] = '<select class="form-control custom-select" name="" id="menu-' . $menu_name . '" onChange="window.document.location.href=this.options[this.selectedIndex].value;">';
-    $data['menu_list'] .= '<option value="">Select a language</option>';
+    $data['menu_list'] .= '<option value="">View this text in different language</option>';
     foreach ( (array) $menu_items as $key => $menu_item ) {
         $title = $menu_item->title;
         $url = $menu_item->url;
