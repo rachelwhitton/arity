fd053e121c3e3ffb62125f1e8fe631321de4dfa8
Usman Khawaja <ukhawaja@vsapartners.com>
2019-01-25 12:57:45 -0500

    Added blog page Popup

--- web/wp-content/themes/arity/page-popup.php
+++ web/wp-content/themes/arity/page-popup.php
@@ -5,8 +5,20 @@ namespace App\Theme;
 use function App\Theme\template;
 
 session_start();
-echo session_id();
-$_SESSION['popup'] = 'true';
+$_SESSION['myCookie'] = $_GET['myCookie'];
+$_SESSION['id'] = session_id();
+if ($_SESSION['showPopup'] == ''){
+    $_SESSION['showPopup'] = 1;
+}
 
-echo "popup";
-echo '<pre>'; print_r($_SESSION); echo '</pre>';
+//https://dev.arity/popup/?a=update&showPopup=0
+if ($_GET['a'] == 'update'){
+    $_SESSION['showPopup'] = $_GET['showPopup'];
+}
+
+//https://dev.arity/popup/?a=destroy
+if ($_GET['a'] == 'destroy'){
+    session_destroy();
+}
+
+echo json_encode($_SESSION);
