f88d36bbdf5c3f5aa459142f7e8937afdbc7bbea
Usman Khawaja <ukhawaja@vsapartners.com>
2019-01-25 18:18:24 -0500

    Updated error msg

--- web/wp-content/themes/arity/resources/templates/page-templates/blog-popup.php
+++ web/wp-content/themes/arity/resources/templates/page-templates/blog-popup.php
@@ -21,17 +21,17 @@
           <div class="form-group form-group--required">
             <label class="form-group-label" for="input_first_name">First name</label>
             <input type="text" class="form-control" name="first_name" id="input_first_name" placeholder="" required="" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
-            <div class="form-control-feedback" data-error="required">*</div>
-            <div class="form-control-feedback" data-error="invalid">*</div>
+            <div class="form-control-feedback" data-error="required">Required</div>
+            <div class="form-control-feedback" data-error="invalid">Invalid</div>
           </div>
         </div>
-        <div>&nbsp;</div>
+        <div>&nbsp;&nbsp;</div>
         <div style="flex:1">
           <div class="form-group form-group--required">
             <label class="form-group-label" for="input_last_name">Last name</label>
             <input type="text" class="form-control" name="last_name" id="input_last_name" placeholder="" required="">
-            <div class="form-control-feedback" data-error="required">*</div>
-            <div class="form-control-feedback" data-error="invalid">*</div>
+            <div class="form-control-feedback" data-error="required">Required</div>
+            <div class="form-control-feedback" data-error="invalid">Invalid</div>
           </div>
         </div>
       </div>
