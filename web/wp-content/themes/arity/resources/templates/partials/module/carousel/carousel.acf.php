09efa1a08319a141914c0316b47cd9772f05255e
Alberto Cristancho <acristancho@vsapartners.com>
2019-06-12 17:41:49 -0400

    Updates to carousel module transition duration

--- web/wp-content/themes/arity/resources/templates/partials/module/carousel/carousel.acf.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/carousel/carousel.acf.php
@@ -32,31 +32,31 @@ $fields = [
 		'label' => 'Carousel slides',
 		'name' => 'carousel__carousel-items',
 		'sub_fields' => [
-			// image
-      acf_image([
-        'label' => 'Images',
-        'name' => 'image_id',
-        'key' => 'field_image_id',
-        'return_format' => 'id',
-        'instructions' => 'Suggested image size: <br>2400 &times; 1286 px',
-        'required' => 1,
-        'preview_size'  => 'thumbnail',
-        'wrapper' => array (
-          'width' => '33',
-        ),
-      ]),
-      // caption
-      acf_text([
-    		'label' => 'Captions',
-    		'name' => 'image_caption',
-    		'key' => 'field_image-caption',
-    		'instructions' => 'Recommended character count max: 120',
-    		'required' => 0,
-    		'maxlength' => '',
-    		'wrapper' => array(
-    			'width' => '66',
-    		),
-      ]),
+		// image
+		acf_image([
+			'label' => 'Images',
+			'name' => 'image_id',
+			'key' => 'field_image_id',
+			'return_format' => 'id',
+			'instructions' => 'Suggested image size: <br>2400 &times; 1286 px',
+			'required' => 1,
+			'preview_size'  => 'thumbnail',
+			'wrapper' => array (
+				'width' => '33',
+			),
+		]),
+		// caption
+		acf_text([
+			'label' => 'Captions',
+			'name' => 'image_caption',
+			'key' => 'field_image-caption',
+			'instructions' => 'Recommended character count max: 120',
+			'required' => 0,
+			'maxlength' => '',
+			'wrapper' => array(
+				'width' => '66',
+			),
+		]),
 		],
 		'button_label' => 'Add slide',
 	]),
@@ -65,17 +65,17 @@ $fields = [
 acf_field_group([
 	'title' => 'Module - Block: Carousel',
 	'name' => 'module__carousel',
-  'key' => 'group_module_carousel',
-  'fields' => $fields,
-  'location' => [
-    [
+	'key' => 'group_module_carousel',
+	'fields' => $fields,
+	'location' => [
+		[
 			acf_location('post_status', 'inactive')
-    ]
-  ],
-  'hide_on_screen' => [
-    'the_content',
-    'custom_fields',
-    'format',
-    'featured_image'
-  ]
+		]
+	],
+	'hide_on_screen' => [
+    	'the_content',
+    	'custom_fields',
+    	'format',
+    	'featured_image'
+  	]
 ]);
