cfbaa0ac4c03e967fe7173ff12db46904fed07ab
Alberto Cristancho <acristancho@vsapartners.com>
2019-09-06 14:20:13 -0400

    Updated careers RSS feed to return Arity-specific jobs

--- web/wp-content/themes/arity/resources/templates/partials/module/career-list/career-list.html.php
+++ web/wp-content/themes/arity/resources/templates/partials/module/career-list/career-list.html.php
@@ -9,7 +9,7 @@ namespace App\Theme;
   Last Updated:       08/01/2017
   Since:              1.0.0
 */
-//echo '<pre>';print_r($data);echo '</pre>';
+// echo '<pre>';print_r($data['raw_feed']['job']);echo '</pre>';
 ?>
 <div <?php module_class('career-list'); ?>>
   <div class="container">
