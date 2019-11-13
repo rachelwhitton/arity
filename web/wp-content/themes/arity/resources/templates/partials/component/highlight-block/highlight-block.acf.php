7fca508e5e4f319709bc8620299afe7784d2f1df
Alberto Cristancho <acristancho@vsapartners.com>
2019-11-13 12:11:40 -0500

    Updates to highlight block component to fix link button icon placement

--- web/wp-content/themes/arity/resources/templates/partials/component/highlight-block/highlight-block.acf.php
+++ web/wp-content/themes/arity/resources/templates/partials/component/highlight-block/highlight-block.acf.php
@@ -4,85 +4,102 @@ namespace App\Theme;
 // ACF Fields
 $fields = [
 
-    // Image
-    acf_image([
-      'label' => 'Image',
-      'name' => 'highlight-block__image_id',
-      'key' => 'field_image',
-      'return_format' => 'id',
-      'instructions' => 'Suggested image size: 192 x 192px',
-      'required' => 1,
-      'preview_size'  => 'thumbnail',
-      'wrapper' => array (
-        'width' => '33',
-      )
-    ]),
+  // Image
+  acf_image([
+    'label' => 'Image',
+    'name' => 'highlight-block__image_id',
+    'key' => 'field_image',
+    'return_format' => 'id',
+    'instructions' => 'Suggested image size: 192 x 192px',
+    'required' => 1,
+    'preview_size'  => 'thumbnail',
+    'wrapper' => array (
+      'width' => '33',
+    )
+  ]),
+  // Subhead
+  acf_text([
+    'label' => 'Subhead',
+    'name' => 'highlight-block__subhead',
+    'key' => 'field_subhead',
+    'instructions' => 'Recommended max character count: 45<br/> Ideally this would wrap no more than twice on any device.',
+    'required' => 1,
+    'maxlength' => 70,
+    'wrapper' => array (
+      'width' => '33',
+    ),
+  ]),
+  // Textarea
+  acf_textarea([
+    'label' => 'Body Copy',
+    'name' => 'highlight-block__body_copy',
+    'key' => 'field_body_copy',
+    'instructions' => 'Recommended max character count: 140<br/> Because copy here is center-aligned, try to keep it as short as possible.',
+    'maxlength' => 190,
+    'required' => 0,
+    'new_lines' => 'wpautop'
+  ]),
+  // CTA
+  acf_link([
+    'label' => 'CTA Link Button',
+    'name' => 'highlight-block__cta',
+    'key' => 'field_cta',
+    'instructions' => '',
+    'required' => 0,
+    'wrapper' => array(
+      'width' => '33',
+    ),
+  ]),
+  // CTA icon
+  acf_select([
+    'label' => 'CTA Link Icon',
+    'name' => 'highlight-block__cta-icon',
+    'key' => 'field_cta-icon',
+    'instructions' => '',
+    'required' => 0,
+    'maxlength' => '',
+    'allow_null' => 0,
+    'ui' => 1,
+    'default_value' => 'arrow-right',
+    'choices' => [
+      'arrow-right' => 'Link',
+      'download' => 'Download',
+      'external' => 'External',
+      'email' => 'Email',
+    ],
+    'wrapper' => array(
+      'width' => '33',
+    ),
+  ]),
+  // CTA link text width
+  acf_text([
+    'label' => 'CTA Link Text Width',
+    'name' => 'highlight-block__link-text-width',
+    'key' => 'field_highlight-block_link-text-width',
+    'instructions' => 'Enter the desired width, as a percentage of the block width (e.g., 75%). If left empty, the value will default to ‘auto’.',
+    'required' => 0,
+    'wrapper' => array(
+      'width' => '33',
+    ),
+  ]),
 
-    // Subhead
-    acf_text([
-      'label' => 'Subhead',
-      'name' => 'highlight-block__subhead',
-      'key' => 'field_subhead',
-      'instructions' => 'Recommended max character count: 45<br/> Ideally this would wrap no more than twice on any device.',
-      'required' => 1,
-      'maxlength' => 70,
-      'wrapper' => array (
-        'width' => '33',
-      ),
-    ]),
-
-    // Textarea
-    acf_textarea([
-      'label' => 'Body Copy',
-      'name' => 'highlight-block__body_copy',
-      'key' => 'field_body_copy',
-      'instructions' => 'Recommended max character count: 140<br/> Because copy here is center-aligned, try to keep it as short as possible.',
-      'maxlength' => 190,
-      'required' => 0,
-      'new_lines' => 'wpautop'
-    ]),
-
-    // CTA
-    acf_link([
-      'label' => 'CTA Button',
-      'name' => 'highlight-block__cta',
-      'key' => 'field_cta',
-      'instructions' => '',
-      'required' => 0
-    ]),
-    acf_select([
-      'label' => 'CTA Icon',
-      'name' => 'highlight-block__cta-icon',
-      'key' => 'field_cta-icon',
-      'instructions' => '',
-      'required' => 0,
-      'maxlength' => '',
-      'allow_null' => 0,
-      'ui' => 1,
-      'default_value' => 'arrow-right',
-      'choices' => [
-        'arrow-right' => 'Link',
-        'download' => 'Download',
-        'external' => 'External'
-      ]
-    ])
 ];
 
 // ACF Field Group
 acf_field_group([
-    'title' => 'Component - Highlight Block',
-    'name' => 'component__highlight-block',
-    'key' => 'group_component_highlight-block',
-    'fields' => $fields,
-    'location' => [
-        [
-            acf_location('post_status', 'inactive')
-        ]
-    ],
-    'hide_on_screen' => [
-        'the_content',
-        'custom_fields',
-        'format',
-        'featured_image'
-    ]
+  'title' => 'Component - Highlight Block',
+  'name' => 'component__highlight-block',
+  'key' => 'group_component_highlight-block',
+  'fields' => $fields,
+  'location' => [
+      [
+          acf_location('post_status', 'inactive')
+      ]
+  ],
+  'hide_on_screen' => [
+      'the_content',
+      'custom_fields',
+      'format',
+      'featured_image'
+  ]
 ]);
